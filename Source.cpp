1493eda49ca94fda5f1093f6a5fb06966f2bbfce
Anduin Lotar <Dmitriy.cronos@gmail.com>
2024-01-16 17:47:40 +0600

    New

---
+++ Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath> 
+
+class Vector
+{
+private:
+	double x;
+	double y;
+	double z;
+
+public:
+	Vector(double x, double y, double z) : x(x), y(y), z(z) {}
+
+	void print()
+	{
+		std::cout << "Vector(" << x << ", " << y << ", " << z << ")" << std::endl;
+    }
+
+	double length()
+	{
+		return std::sqrt(x * x + y * y + z * z);
+	}
+};
+
+int main()
+{
+	Vector v(1.0, 2.0, 3.0);
+	v.print();
+	std::cout << "Length: " << v.length() << std::endl;
+
+	return 0;
+}
